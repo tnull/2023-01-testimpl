650292aa51fd440838b59333a3d087d5fdd6b0c6
Elias Rohrer <ero@tnull.de>
2023-01-25 20:23:22 -0600

    f Actually write the message out

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
 	use lightning::impl_writeable_msg;
+	use lightning::util::ser::Writeable;
 	struct MyCustomMessage {
 		pub field_1: u32,
 		pub field_2: bool,
@@ -14,4 +15,11 @@ fn main() {
 	}, {
 		(1, tlv_optional_integer, option),
 	});
+
+	let my_custom_msg = MyCustomMessage { field_1: 1, field_2: true, field_3: "A".to_owned(), tlv_optional_integer: Some(1) };
+
+
+	let mut my_vec: Vec<u8> = Vec::new();
+	my_custom_msg.write(&mut my_vec).unwrap();
+	println!("{:?}", my_vec);
 }
